d39f942276311c5afc5ca571d9dc00f8a255e0d4
Carol Kosgei <caroljemutai35@gmail.com>
2025-03-31 12:59:36 +0300

    adds arrow-function

---
+++ index.js
@@ -0,0 +1,20 @@
+function divide () {
+    result(divide)
+}
+
+function divide (a,b) {
+    a= 2000
+    b= 100
+    return(a/b)
+}
+
+function square (x)  {
+     x= 2
+    return (x)*2
+}
+
+function add (a,b)  {
+     a= 3
+     b= 4
+    return(a+b)
+}
